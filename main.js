de62cc4d1f22cdb4105768f1cd6fb315dcc531fc
Faqih Ikhtibar <faqihikhtibar@gmail.com>
2026-03-09 10:31:35 +0700

    init setup local storage

--- main.js
+++ main.js
@@ -1,2 +1,16 @@
 // Do your work here...
-console.log('Hello, world!');
+const  STORAGE_KEY = "BOOKSHELF_DATA";
+
+let bookCollection = [];
+
+function saveData() {
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(bookCollection));
+}
+
+function loadData() {
+  const savedData = localStorage.getItem(STORAGE_KEY);
+
+  if (savedData !== null) {
+    bookCollection = JSON.parse(savedData);
+  }
+}
